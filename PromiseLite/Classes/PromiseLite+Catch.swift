d1b7680a3640eb05095a7648628fe5b31be69c00
François Rouault <francois.rouault@cocoricostudio.com>
2020-05-17 01:18:51 +0200

    Add catch function to handle rejection

---
+++ PromiseLite/Classes/PromiseLite+Catch.swift
@@ -0,0 +1,28 @@
+//
+//  PromiseLite+Catch.swift
+//  PromiseLite
+//
+//  Created by François Rouault on 17/05/2020.
+//
+
+extension PromiseLite {
+  /// Returns a Promise and deals with rejected cases only.
+  ///
+  /// The Promise returned by `catch()` is rejected if the rejection block throws an error or returns a Promise which is itself rejected; otherwise, it is resolved.
+  /// - Parameter rejection: A completion block that is called when the Promise is rejected.
+  @discardableResult
+  public func flatCatch(_ rejection: @escaping (Error) throws -> PromiseLite) -> PromiseLite {
+    flatMap({ value in PromiseLite.resolve(value) },
+            rejection: rejection)
+  }
+
+  /// Returns a Promise and deals with rejected cases only.
+  ///
+  /// The Promise returned by `catch()` is rejected if the rejection block throws an error or returns a Promise which is itself rejected; otherwise, it is resolved.
+  /// - Parameter rejection: A completion block that is called when the Promise is rejected.
+  @discardableResult
+  public func `catch`(_ rejection: @escaping (Error) throws -> Value) -> PromiseLite {
+    flatMap({ value in PromiseLite.resolve(value) },
+            rejection: { PromiseLite.resolve(try rejection($0)) })
+  }
+}
---
+++ Tests/CatchTests.swift
@@ -0,0 +1,57 @@
+//
+//  CatchTests.swift
+//  Pods-PromiseLite_Tests
+//
+//  Created by François Rouault on 26/04/2020.
+//
+
+import XCTest
+import PromiseLite
+
+private typealias Promise = PromiseLite
+
+class CatchTests: XCTestCase {
+  func test_reject_can_be_catched_then_continue() {
+    // given
+    var theError: Error?
+    var theResult: String?
+
+    // when
+    Promise<String>.reject(FooError.💥)
+      .flatCatch { error -> Promise<String> in theError = error; return Promise.resolve("error handled") }
+      .map { aString in theResult = aString }
+
+    // then
+    XCTAssertEqual(theError as? FooError, FooError.💥)
+    XCTAssertEqual(theResult, "error handled")
+  }
+
+  func test_catch_return_a_value() {
+    // given
+    var theError: Error?
+    var theResult: String?
+
+    // when
+    Promise<String>.reject(FooError.💥)
+      .catch { error in theError = error; return "error handled" }
+      .map { aString in theResult = aString }
+
+    // then
+    XCTAssertEqual(theError as? FooError, FooError.💥)
+    XCTAssertEqual(theResult, "error handled")
+  }
+
+  func test_catch_rejection_block_is_ignored_when_promise_resolves() {
+    // given
+    let promiseThatResolve = Promise.resolve(7)
+    var result: Int?
+
+    // when
+    promiseThatResolve
+      .catch { _ in 3 }
+      .map { result = $0 }
+
+    // then
+    XCTAssertEqual(result, 7)
+  }
+}
